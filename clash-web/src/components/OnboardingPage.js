3811554f1aeb99927d70ab9327dd0cad67a4bd14
Sachin v Anchan <104090322+marcilinous@users.noreply.github.com>
2026-06-18 10:39:07 +0530

    Update OnboardingPage.js

--- clash-web/src/components/OnboardingPage.js
+++ clash-web/src/components/OnboardingPage.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useAuth } from "@/lib/auth";
+import { uploadPhoto } from "@/lib/storage";
 import Button from "@/components/ui/Button";
 import Input from "@/components/ui/Input";
 import ChipSelector from "@/components/ui/ChipSelector";
